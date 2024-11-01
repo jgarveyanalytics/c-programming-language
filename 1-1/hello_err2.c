d808f59fd2ceb54c57f350f586dd918c7dccfcd5
Jonathan <j.garvey5@gmail.com>
2024-11-01 19:48:47 -0400

    Exercise 1-1

---
+++ 1-1/hello_err2.c
@@ -0,0 +1,14 @@
+/*
+ * Variation of hello.c from Chapter 1.
+ * main() method closing curly bracket removed.
+ *
+ * Jonathan Garvey
+ * 11/1/2024
+ */
+
+# include <stdio.h>
+
+int main()
+{
+	printf("hello, world\n");
+
---
+++ 1-1/hello_err3.c
@@ -0,0 +1,14 @@
+/*
+ * Variation of hello.c from Chapter 1.
+ * Return type removed from main() method.
+ *
+ * Jonathan Garvey
+ * 11/1/2024
+ */
+
+# include <stdio.h>
+
+main()
+{
+	printf("hello, world\n");
+}
